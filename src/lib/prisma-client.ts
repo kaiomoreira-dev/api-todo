99fcc3d64938ca3979cb31f52321af040c3aa83d
Kaio Moreira <kaio-dev@outlook.com>
2023-10-01 20:02:42 -0300

    fix: prisma/client

--- src/lib/prisma-client.ts
+++ src/lib/prisma-client.ts
@@ -1,5 +1,5 @@
-import { env } from '@/env'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
+    // log: env.NODE_ENV === 'development' ? ['query'] : [],
 })
