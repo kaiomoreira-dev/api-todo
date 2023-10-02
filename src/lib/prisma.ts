5b36183c616e485d5565ac37fe14fd6b50d9870e
Kaio Moreira <kaio-dev@outlook.com>
2023-10-02 11:13:05 -0300

    restore: prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,3 @@
-import { env } from "@/env";
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
-    // log: env.NODE_ENV === 'development' ? ['query'] : [],
-})
+export const prisma = new PrismaClient()
