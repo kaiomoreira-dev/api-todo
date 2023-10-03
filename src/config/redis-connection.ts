0723d08fababed0e57071e38a6d367e2f8e4e687
Kaio Moreira <kaio-dev@outlook.com>
2023-10-02 21:54:19 -0300

    modify: redis-connection for execute in production and development

--- src/config/redis-connection.ts
+++ src/config/redis-connection.ts
@@ -1,9 +1,10 @@
+import { env } from "@/env";
 import "dotenv/config";
 import { createClient } from "redis";
 
-export const redisClient = createClient({
-    url: process.env.REDIS_URL,
-});
+export const redisClient = env.NODE_ENV === "development" ?
+    createClient() :
+    createClient({url: env.REDIS_URL})
 
 redisClient.connect()
 .then(()=>{
