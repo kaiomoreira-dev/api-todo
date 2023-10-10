6bca16583e3ce244f4a7304619d948179b5fa7ef
Kaio Moreira <kaio-dev@outlook.com>
2023-10-10 14:25:27 -0300

    alter: cors for received method patch

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ export const fastifyApp = fastify()
 fastifyApp.register(fastifyCors, {
     origin: true,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   })
 
 fastifyApp.register(usersRoutes,{
