b6a466c9cfd7c2719ed7f9cc6146a4638cd903bc
Kaio Moreira <kaio-dev@outlook.com>
2023-10-10 16:02:32 -0300

    add: error handler in refresh token timeout token

--- src/http/controllers/users/refresh-token/refresh-token-users-controller.ts
+++ src/http/controllers/users/refresh-token/refresh-token-users-controller.ts
@@ -1,3 +1,4 @@
+import { AccessTimeOutError } from "@/usecases/errors/access-time-out-error";
 import { ResourceNotFoundError } from "@/usecases/errors/resource-not-found-error";
 import { makeRefreshToken } from "@/usecases/factories/users/make-refresh-token-usecase";
 import { FastifyReply, FastifyRequest } from "fastify";
@@ -20,7 +21,10 @@ export async function RefreshToken (request: FastifyRequest, reply:FastifyReply)
         return reply.status(200).send(updatedTokens)
     } catch (error) {
         if(error instanceof ResourceNotFoundError){
-            return reply.status(401).send({ message: error.message})
+            return reply.status(404).send({ message: error.message})
         }
+        if(error instanceof AccessTimeOutError){
+            return reply.status(401).send({ message: error.message})
+          }
     }
 }
