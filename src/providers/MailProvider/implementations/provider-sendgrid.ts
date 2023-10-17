c109dc5073a861a8052648e180661089ef4d23b5
Kaio Moreira <kaio-dev@outlook.com>
2023-10-17 17:24:53 -0300

    alter: send email for emails dynamic

--- src/providers/MailProvider/implementations/provider-sendgrid.ts
+++ src/providers/MailProvider/implementations/provider-sendgrid.ts
@@ -30,7 +30,7 @@ export class MailProvider implements IMailProvider{
             const htmlTemplate = compileTemplate({name, link, email});
 
             const msg = {
-                to: 'kaio-dev@outlook.com', // Para 
+                to: email, // Para 
                 from: 'naoresponda@kaiomoreira-dev.com.br', // De
                 subject: subject, // Assunto
                 html: htmlTemplate,
